de2419f89da138023dedcb15b591da1fe86ade22
IchiTai <ichitai0513@icloud.com>
2024-10-16 02:10:02 +0900

    add savefunction

--- lib/category_provider.dart
+++ lib/category_provider.dart
@@ -1,14 +1,42 @@
-import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
 import 'item_model.dart';
+import 'package:flutter/foundation.dart';
 
 class CategoryProvider with ChangeNotifier {
   Map<String, List<Item>> _categories = {};
 
   Map<String, List<Item>> get categories => _categories;
 
+  Future<void> saveCategories() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    Map<String, dynamic> jsonCategories = _categories.map((key, value) {
+      return MapEntry(key, value.map((item) => item.toJson()).toList());
+    });
+
+    prefs.setString('categories', jsonEncode(jsonCategories));
+  }
+
+  Future<void> loadCategories() async {
+    final prefs = await SharedPreferences.getInstance();
+    final String? categoriesString = prefs.getString('categories');
+    if (categoriesString != null) {
+      final Map<String, dynamic> decodedCategories =
+          jsonDecode(categoriesString);
+
+      _categories = decodedCategories.map((key, value) {
+        return MapEntry(
+            key, (value as List).map((item) => Item.fromJson(item)).toList());
+      });
+      notifyListeners();
+    }
+  }
+
   void addCategory(String category) {
     if (!_categories.containsKey(category)) {
       _categories[category] = [];
+      saveCategories();
       notifyListeners();
     }
   }
@@ -16,12 +44,20 @@ class CategoryProvider with ChangeNotifier {
   void addItem(String category, String itemName, int quantity) {
     if (_categories.containsKey(category)) {
       _categories[category]?.add(Item(itemName, quantity));
+      saveCategories();
       notifyListeners();
     }
   }
 
   void removeCategory(String category) {
     _categories.remove(category);
+    saveCategories();
+    notifyListeners();
+  }
+
+  void removeItem(String category, String itemName) {
+    _categories[category]?.removeWhere((item) => item.name == itemName);
+    saveCategories();
     notifyListeners();
   }
 
@@ -30,12 +66,8 @@ class CategoryProvider with ChangeNotifier {
         _categories[category]?.firstWhere((item) => item.name == itemName);
     if (item != null) {
       item.quantity = newQuantity;
+      saveCategories();
       notifyListeners();
     }
-  }
-
-  void removeItem(String category, String itemName) {
-    _categories[category]?.removeWhere((item) => item.name == itemName);
-    notifyListeners();
   }
 }
--- lib/item_model.dart
+++ lib/item_model.dart
@@ -3,4 +3,18 @@ class Item {
   int quantity;
 
   Item(this.name, this.quantity);
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'quantity': quantity,
+    };
+  }
+
+  factory Item.fromJson(Map<String, dynamic> json) {
+    return Item(
+      json['name'],
+      json['quantity'],
+    );
+  }
 }
